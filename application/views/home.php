3da100321b15552d3f3b7f3f3f639cba81877df5
Tharindu Sandaruwan <tharindusandaruwan40@gmail.com>
2018-03-28 13:26:20 +0530

    Tharindu

---
+++ application/views/home.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thari
+ * Date: 2018-03-28
+ * Time: PM 1.01
+ */
---
+++ application/views/templates/footer.php
@@ -0,0 +1,58 @@
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 footer-top-left">
+                    <p><i class="fa fa-phone" aria-hidden="true"></i> +94 71 171 8822</p>
+                </div>
+                <div class="col-md-6 footer-top-left">
+                    <p><i class="fa fa-envelope" aria-hidden="true"></i> Email :<a href="mailto:example@email.com"> teamforesight@gmail.com</a></p>
+                </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-bottom-grids">
+                <div class="row">
+                    <div class="col-md-3 footer-bottom-right">
+                        <h5 style="text-align: center;">Contact Us</h5>
+
+                        <p style="text-align: center;">Suspendisse potenti. Pellentesque pulvinar tellus at est ullamcorper, at elementum nibh laoreet.</p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-md-4 footer-bottom-right">
+                        <h5 style="text-align: center;">Newsletter</h5>
+
+                        <form action="#" method="post" >
+                            <input type="text" placeholder="Subscribe" name="Subscribe" required="">
+                            <input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-md-3 footer-bottom-right">
+                        <h5 style="text-align: center;">We are social</h5>
+                        <div class="agileinfo-social-grids" style="text-align: center;">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+            </div>
+            <br>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <p>Copyright © 2018. All rights reserved | Design by <a href="https://www.facebook.com/TeamForesight1/">Team Foresight</a></p>
+        </div>
+    </div>
+</footer>
+
+<script src="<?php echo base_url().'assets/js/jquery.min.js';?>" type="text/javascript"></script>
+<script src="<?php echo base_url().'assets/js/bootstrap.min.js';?>" type="text/javascript"></script>
