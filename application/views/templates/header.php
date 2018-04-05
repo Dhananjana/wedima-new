495d376e80fbda9a288d2d6fe0ec3bbb40138f75
warunikaprimali <diupamali@gmail.com>
2018-04-06 01:59:08 +0530

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
     <title>The Wedima</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link href="<?php echo base_url().'assets/css/vendor.css';?>" rel="stylesheet" type="text/css" media="all">
+    <link href="<?php echo base_url().'assets/css/layout.css';?>" rel="stylesheet" type="text/css" media="all">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/themes/base/jquery-ui.css" type="text/css" media="all" />  
@@ -32,28 +32,46 @@
 
                 <li class="nav-item">
                     <a class="nav-link font-clr" href="contact.html">Special Offers</a>
-                </li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </li>&nbsp;&nbsp;
                 <?php
-                    if($this->session->userdata('logged')==1){
+                if($this->session->userdata('logged')==1){
                     ?>
-                    <li class="nav-item">
-                        <a class="nav-link font-clr" href="<?php echo base_url() . 'login/logout'; ?>">Logout <i class="fa fa-sign-out" ></i></a>
-                    </li>
                     <?php
-
-                }
-                    else {
+                    if($this->session->userdata('utype')=='vendor') {
                         ?>
-                        <li class="nav-item">
-                            <a class="nav-link font-clr" href="<?php echo base_url() . 'login'; ?>"><i class="fa fa-sign-in" aria-hidden="true"></i> Sign In</a>
-                        </li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-                        <li class="nav-item">
-                            <a class="nav-link font-clr" href="contact.html">Sign Up</a>
+                        <li class="nav-item" style="color: white">
+                            <a href="<?php echo base_url() . 'vendor'; ?>" style="text-decoration: none">
+                                <img src="<?php echo base_url() . 'assets/images/v1.jpg' ?>" alt="Avatar" class="avatar"
+                                     style="vertical-align: middle;width: 25px;height: 25px;border-radius: 50%;">&nbsp;&nbsp;<?php
+                                echo $this->session->userdata('username');
+                                ?></a>
                         </li>
+
                         <?php
                     }
+                        ?>
+                    <li class="nav-item">
+                        <a class="nav-link font-clr" href="<?php echo base_url().'login/logout';?>"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>
+
+                    </li>
+                    <?php
+                }
+
+                else{
+
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link font-clr" href="<?php echo base_url() . 'login'; ?>"><i class="fa fa-sign-in" aria-hidden="true"></i> Sign In</a>
+                    </li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                    <li class="nav-item">
+                        <a class="nav-link font-clr" href="contact.html">Sign Up</a>
+                    </li>
+                    <?php
+                }
                 ?>
+
 
             </ul>
 
