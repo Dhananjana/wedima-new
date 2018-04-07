18dcffd47520952271cb2a853c501ab33a0d214e
Tharinducs <tharindusandaruwan40@gmail.com>
2018-04-07 08:18:35 +0530

    one backend function for gallery

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -9,9 +9,16 @@
 class Home_model extends CI_Model
 {
     public function get_testinominals(){
-        $this->db->order_by('id',"ASC");
+        $this->db->order_by('id',"DESC");
         $this->db->limit(3);
         $query=$this->db->get('feedback');
+        return $query->result();
+    }
+
+    public function get_images(){
+        $this->db->order_by('id',"DESC");
+        $this->db->limit(12);
+        $query=$this->db->get('gallery');
         return $query->result();
     }
 }
