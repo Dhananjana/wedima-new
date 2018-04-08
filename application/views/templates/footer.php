334c5e3f3b690ccecbdaca890ed4015117f4b7c9
Dhananjana Milinda <dhananjana.milinda.kumarasiri@gmail.com>
2018-04-08 14:50:12 +0530

    no need this jquery use the jquery you need in our own page

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -56,7 +56,7 @@
 
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 
 
 </body>
